c9ab58ec91e44da2d233fed7e4414e74650f4a47
sarang <twolovelove2232@gmail.com>
2023-03-04 22:49:17 +0900

    comment

--- src/component/CommentsSection.jsx
+++ src/component/CommentsSection.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default function CommentsSection() {
   return (
     <>
-      <h1 className="text-center mt-24 md:mt-32 text-xl md:text-3xl font-bold">
+      <h1 className="text-center mt-24 md:mt-32 text-lg md:text-3xl font-bold">
         빠른 성장 보다 큰 성장을 하고 싶습니다.
       </h1>
       <p className="text-center mt-10 mb-32 text-sm md:text-lg mx-auto">
         사람과 사람사이를 이어주는 것,
         <br />
         그리고 보다 나은 생활과 환경을 만드는 것<br />
-        이보다 더 좋은 개발자로써의 목표는 없다고 생각합니다.
+        이보다 좋은 개발자로써의 목표는 없다고 생각합니다.
         <br />
         신입 개발자로서 많이 배우고 성장하고 싶습니다 😃
       </p>
