1f08e623128777d3f5a324c01d543806e5a37ce9
sarang <twolovelove2232@gmail.com>
2023-03-05 22:33:19 +0900

    deleteusestate

--- src/hooks/useComment.js
+++ src/hooks/useComment.js
@@ -1,5 +1,4 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useState } from 'react';
 import { deleteContact, viewContact } from '../api/firebase';
 
 export default function useComment() {
